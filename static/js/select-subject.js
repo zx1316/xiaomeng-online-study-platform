b64bcf41ea706e060a93b62de300feeb186dafe6
zx1316 <1315862279@qq.com>
2024-10-30 11:19:15 +0800

    加入导航栏和样式优化

--- static/js/select-subject.js
+++ static/js/select-subject.js
@@ -1,65 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const username = document.getElementById('username');
+    const uid = document.getElementById('uid');
+    const logoutLink = document.getElementById('logout-link');
     function showSubjects(subjects){
-
         // 获取要插入内容的容器
-        var listGroup = document.getElementById('subject-list');
+        const listGroup = document.getElementById('subject-list');
 
         // 遍历数据，创建HTML元素并插入到容器中
         subjects.forEach(function(item) {
             // 创建a标签
-            var a = document.createElement('a');
+            const a = document.createElement('a');
             a.href = `/subject-exercise.html?querysubject=${item.Subject}`;
             a.className = "list-group-item list-group-item-action d-flex gap-3 py-3";
             a.target = "_blank";
             a.setAttribute("aria-current", "true");
-
-            // 创建img标签
-            var img = document.createElement('img');
-            img.src = `/img/subject/${item.subject}.png`;
-            img.alt = item.subject;
-            img.width = 64;
-            img.height = 64;
-            img.className = "rounded-circle flex-shrink-0";
-
-            // 创建div容器
-            var div = document.createElement('div');
-            div.className = "d-flex align-items-center gap-3 w-100";
-
-            // 创建内部div
-            var innerDiv = document.createElement('div');
-            innerDiv.className = "flex-grow-1 d-flex flex-column gap-2"
-
-            // 创建h6标签
-            var h6 = document.createElement('h4');
-            h6.className = "mb-0";
-            h6.textContent = item.Subject;
-
-            // 创建p标签
-            var p = document.createElement('p');
-            p.className = "mb-0 opacity-75";
-            if(item.Total!=0){
-                p.textContent = `总体数:${item.Total}, 正确率:${item.Right / item.Total}`
-            }else{
-                p.textContent = `总体数:${item.Total}, 正确率:无`
-            }
-
-
-            // 将h6和p标签添加到内部div中
-            innerDiv.appendChild(h6);
-            innerDiv.appendChild(p);
-
-            // 将img和内部div添加到外层div中
-            div.appendChild(img);
-            div.appendChild(innerDiv);
-
-
-            // 将div添加到a标签中
-            a.appendChild(div);
-
+            a.innerHTML = `
+                <div class="d-flex align-items-center gap-3 w-100">
+                    <img src="/img/subject/${item.Subject}.png" alt="${item.Subject}" width="64" height="64" class="rounded-circle flex-shrink-0">
+                    <div class="flex-grow-1 d-flex flex-column gap-2">
+                        <h4 class="mb-0">${item.Subject}</h4>
+                        <p class="mb-0 opacity-75">练习数: ${item.Total}，正确率: ${item.Total === 0 ? '无' : item.Right / item.Total}</p>
+                    </div>
+                    <i class="fa fa-angle-right opacity-75" style="font-size: 24px"></i>
+                </div>
+            `;
             // 将a标签添加到容器中
             listGroup.appendChild(a);
         });
     }
+
+    function getCookie(name) {
+        const cookieArray = document.cookie.split(';'); // 分割cookie字符串
+        // 遍历cookie数组
+        for (let i = 0; i < cookieArray.length; i++) {
+            const cookiePair = cookieArray[i].trim();   // 获取每个cookie的名称和值
+            const cookieNameValuePair = cookiePair.split('=');  // 分割名称和值
+            // 检查cookie名称是否匹配
+            if (cookieNameValuePair[0] === name) {
+                return decodeURIComponent(cookieNameValuePair[1]);
+            }
+        }
+        return null;
+    }
+
+    // 登出
+    logoutLink.addEventListener('click', (e) => {
+        fetch('/logout', {method: 'POST'})
+            .then(response => response.text())
+            .then(result => {
+                window.location.href = '/signin.html'
+            })
+            .catch(error => {
+                window.location.href = '/signin.html'
+            })
+    });
+
+    username.innerText = getCookie('username');
+    uid.innerText = getCookie('uid');
 
     fetch('subject')
         .then(response => response.json())
