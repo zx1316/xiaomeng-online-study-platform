4311d03d0387364aee39591ab0243af29db02ac3
zx1316 <1315862279@qq.com>
2024-11-12 13:10:20 +0800

    对战选择页面

---
+++ static/js/battle-select.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const username = document.getElementById('username');
+    const uid = document.getElementById('uid');
+    const userAvatar = document.getElementById('user-avatar');
+    const logoutLink = document.getElementById('logout-link');
+    const userDropdownTrigger = document.getElementById('user-dropdown-trigger');
+    const userDropdownMenu = document.getElementById('user-dropdown-menu');
+
+    function getCookie(name) {
+        const cookieArray = document.cookie.split(';'); // 分割cookie字符串
+        // 遍历cookie数组
+        for (let i = 0; i < cookieArray.length; i++) {
+            const cookiePair = cookieArray[i].trim();   // 获取每个cookie的名称和值
+            const cookieNameValuePair = cookiePair.split('=');  // 分割名称和值
+            // 检查cookie名称是否匹配
+            if (cookieNameValuePair[0] === name) {
+                return decodeURIComponent(cookieNameValuePair[1]);
+            }
+        }
+        return null;
+    }
+
+    // 用户下拉栏位置偏移
+    userDropdownTrigger.addEventListener('shown.bs.dropdown', () => {
+        const rect = userDropdownMenu.getBoundingClientRect()
+        if (rect.x + rect.width > window.innerWidth) {
+            userDropdownMenu.style.transform = `translateX(${window.innerWidth - rect.x - rect.width}px)`
+        }
+    });
+    userDropdownTrigger.addEventListener('hidden.bs.dropdown', () => {
+        userDropdownMenu.style.transform = 'none'
+    });
+
+    // 登出
+    logoutLink.addEventListener('click', (e) => {
+        sessionStorage.clear();
+        fetch('/logout', {method: 'POST'})
+            .then(response => response.text())
+            .then(result => {
+                window.location.href = '/signin.html'
+            })
+            .catch(error => {
+                window.location.href = '/signin.html'
+            })
+    });
+
+    username.innerText = getCookie('username');
+    uid.innerText = getCookie('uid');
+    userAvatar.src = `img/user/${uid.innerText}.png`;
+});
